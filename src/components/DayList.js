abc3ba06b4efeef82cd991da5d1b3c79273c417e
amnahasad <amnah_asad@hotmail.com>
2021-12-09 14:36:16 -0500

    Fixed selected day state

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import DayListItem from "./DayListItem";
+import "./DayListItem.scss";
 
 export default function DayList(props) {
 
     const DayListItems = props.days.map((dayData, index) => {
-        return <DayListItem key={index} {...dayData} setDay={props.setDay} />
+        return <DayListItem key={index} selected={props.day===dayData.name} {...dayData} setDay={props.setDay} />
     })
   return (
     <ul>
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,7 +3,6 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 
-
 const formatSpots = function(spots) {
     return (spots === 0 ? "no spots remaining" : `${spots} spot${spots === 1 ? "" : "s"} remaining`);
 };
