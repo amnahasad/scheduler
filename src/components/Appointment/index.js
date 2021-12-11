dcab64806d9c852a16b47317ba61f41ee4332fc9
amnahasad <amnah_asad@hotmail.com>
2021-12-10 19:44:36 -0500

    Added the Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./styles.scss"
+import classNames from 'classnames';
+
+
+export default function Appointment(props) {
+    return(
+        <article className="appointment"></article>
+    );
+}
