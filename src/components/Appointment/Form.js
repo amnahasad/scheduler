ac04e8d4db569fffba4a4d8742877caf8cd57936
amnahasad <amnah_asad@hotmail.com>
2021-12-16 11:25:00 -0500

    Added form input validation and test

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,6 +7,7 @@ export default function Form(props) {
 
     const [student, setStudent] = useState(props.student || "");
     const [interviewer, setInterviewer] = useState(props.interviewer || null);
+    const [error, setError] = useState("");
 
     const reset = () => {
         setStudent(null);
@@ -18,6 +19,16 @@ export default function Form(props) {
     const clickHandler = () => {
         props.onSave(student, interviewer)
     }
+
+    function validate() {
+        if (student === "") {
+          setError("Student name cannot be blank");
+          return;
+        }
+      
+        props.onSave(student, interviewer);
+      }
+    
     return (
         <main className="appointment__card appointment__card--create">
             <section className="appointment__card-left">
@@ -29,6 +40,7 @@ export default function Form(props) {
                         value={student}
                         placeholder="Enter Student Name"
                         onChange={(event) => setStudent(event.target.value)}
+                        data-testid="student-name-input"
                     />
                 </form>
                 <InterviewerList
@@ -40,9 +52,10 @@ export default function Form(props) {
             <section className="appointment__card-right">
                 <section className="appointment__actions">
                     <Button danger onClick={reset}>Cancel</Button>
-                    <Button confirm onClick={clickHandler} >Save</Button>
+                    <Button confirm onClick={validate} >Save</Button>
                 </section>
             </section>
+            <section className="appointment__validation">{error}</section>
         </main>
     );
 }
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -12,7 +12,7 @@ import { cleanup, render } from "@testing-library/react";
 /*
   We import the component that we are testing
 */
-import Appointment from "components/Application";
+import Appointment from "components/Appointment";
 
 // /*
 //   A test that renders a React Component
